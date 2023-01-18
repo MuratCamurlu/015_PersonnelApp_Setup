cf0a84ded660d23333d02085cb18f40bf2d4a7b8
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-01-18 13:51:04 +0300

    persoal view

--- personnel/views.py
+++ personnel/views.py
@@ -1,5 +1,6 @@
 
-from rest_framework import generics
+from rest_framework import generics,status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import DepartmentSerializer,PersonnelSerializer
 from .models import Department,Personnel
@@ -16,4 +17,29 @@ class DepartmentView(generics.ListCreateAPIView):
 class PersonnelListCreateView(generics.ListCreateAPIView):
     serializer_class = PersonnelSerializer
     queryset = Personnel.objects.all()
-    permission_classes=[IsAuthenticated,IsStafforReadOnly]
+   
+    permission_classes = [IsAuthenticated]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if self.request.user.is_staff:
+            personnel = self.perform_create(serializer)
+            data = {
+                "message": f"Personal {personnel.first_name} saved successfully",
+                "personnel" : serializer.data
+            }
+        else:
+            data = {
+                "message": "Yetkiniz yoktur.."
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+        
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        person = serializer.save()
+        person.create_user = self.request.user
+        person.save()
+        return person
